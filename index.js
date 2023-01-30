8801fc39a5085703c7a3cd7b02dbeaace3980e48
JapsG <jaipervlogs@gmail.com>
2023-01-30 13:42:45 +0000

    new address

--- index.js
+++ index.js
@@ -7,29 +7,21 @@ const {
     LAMPORTS_PER_SOL
 } = require("@solana/web3.js");
 
-// Create a new keypair
-const newPair = new Keypair();
 
-// Exact the public and private key from the keypair
-const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
-const privateKey = newPair._keypair.secretKey;
+const myWallet = new PublicKey(process.argv.slice(2).toString());
 
 // Connect to the Devnet
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-console.log("Public Key of the generated keypair", publicKey);
 
 // Get the wallet balance from a given private key
 const getWalletBalance = async () => {
     try {
         // Connect to the Devnet
         const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        console.log("Connection object is:", connection);
-
-        // Make a wallet (keypair) from privateKey and get its balance
-        const myWallet = await Keypair.fromSecretKey(privateKey);
+        //console.log("Connection object is:", connection);
         const walletBalance = await connection.getBalance(
-            new PublicKey(newPair.publicKey)
+            myWallet
         );
         console.log(`Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`);
     } catch (err) {
@@ -41,12 +33,11 @@ const airDropSol = async () => {
     try {
         // Connect to the Devnet and make a wallet from privateKey
         const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const myWallet = await Keypair.fromSecretKey(privateKey);
 
         // Request airdrop of 2 SOL to the wallet
         console.log("Airdropping some SOL to my wallet!");
         const fromAirDropSignature = await connection.requestAirdrop(
-            new PublicKey(myWallet.publicKey),
+            myWallet,
             2 * LAMPORTS_PER_SOL
         );
         await connection.confirmTransaction(fromAirDropSignature);
